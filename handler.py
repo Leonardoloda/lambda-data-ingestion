d46492a1184b9b5c74a4427557c0e2bd38a8c73c
Leonardo <leonardoloda2@gmail.com>
2025-02-22 23:08:00 -0500

    chore: added extra logging

--- handler.py
+++ handler.py
@@ -1,21 +1,35 @@
-from logging import getLogger
+import logging
 
 from extract_step import ExtractStep
 from decode_step import DecodeStep
 from parser_step import ParserStep
 from map_step import MapStep
 
-logger = getLogger()
+logging.basicConfig(format="%(levelname)s:%(name)s:%(message)s", level=logging.INFO)
 
 extract = ExtractStep(queue="RAW-ROSTER-JCT-int-0")
 decode = DecodeStep()
 parser = ParserStep()
 mapper = MapStep()
 
-def handler(event = {}):
-    values = extract.execute(event=event)
-    payloads = decode.execute(values=values)
-    roots = parser.execute(payloads=payloads)
-    entities = mapper.execute(roots=roots)
 
-    print(entities)
+def handler(event={}):
+    try:
+        logging.info("Started lambda for event: %s", event)
+
+        values = extract.execute(event=event)
+        logging.info("Values extracted from event %s", values)
+
+        payloads = decode.execute(values=values)
+        logging.info("Payloads decoded %s", payloads)
+
+        roots = parser.execute(payloads=payloads)
+        logging.info("parsed %s xmls", len(roots))
+
+        entities = mapper.execute(roots=roots)
+
+    except ValueError as e:
+        logging.error("ValueError encountered: %s", e)
+
+
+handler()
